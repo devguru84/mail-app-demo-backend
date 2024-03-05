e2cf3f02402d7f2884aed9e63001a7908207e0ad
DevGuru84 <fullstackdevguru@outlook.com>
2024-03-05 02:13:00 -0500

    Fix localhost port

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(bodyParser.json())
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000")
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080")
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST"
